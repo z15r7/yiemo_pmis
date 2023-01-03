4bae1fa2061b715a866364445be12cfaae1c4901
z15r7 <z15r7x@gmail.com>
2023-01-04 06:02:26 +0800

    update homepage

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,31 +1,9 @@
 <template>
-  <Main />
-  <div>
-    <!-- <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a> -->
+  <div class="flex place-items-center justify-center h-screen">
+    <Main />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <script setup>
-    import HelloWorld from '/src/components/HelloWorld.vue'
     import Main from '/src/components/Main.vue';
 </script>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
