28102eeee69e9ad3719949c74000c5b4d738b2dc
z15r7 <z15r7x@gmail.com>
2023-01-04 06:03:24 +0800

    basic item was added.

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex justify-center place-items-center shadow bg-amber-500 p-2 rounded-md">
+        <div>hbg</div>
+        <div class="w-full">
+            <p class="text-xl font-bold text-center">{{ title }}</p>
+        </div>
+        <div>btn</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        title: "yi.emo_nccu",
+    }),
+    methods: {
+    },
+    computed: {
+    },
+}
+</script>
--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,27 +1,57 @@
 <template>
-    <div class="">
-        <p>{{ x }}</p>
-        <p v-if="x===1111">{{ "x===1111" }}</p>
-        <p v-if="x>1111">{{ showAbc }}</p>
+    <div class="place-item-center w-4/6 p-8 flex-row flex-grow-0 bg-slate-200 h-screen">
+        <Header />
+        <div class="my-4 border border-slate-50 shadow bg-white p-2 rounded-md">{{ x }}</div>
+        <div class="my-4 border border-slate-50 shadow bg-white p-2 rounded-md" v-if="x===1111">{{ "x===1111" }}</div>
+        <div class="my-4 border border-slate-50 shadow bg-white p-2 rounded-md" v-if="x>1111">{{ showAbc }}</div>
+        <div class="flex justify-center my-4 shadow bg-blue-500 rounded-md p-2 text-white">
+            <button class="w-full -m-2 p-2" @click="add">add</button>
+        </div>
+        <div class="flex flex-grow-0 space-x-4">
+            <div class="flex justify-center flex-grow-0 w-full border border-slate-50 shadow bg-white p-2 rounded-md">
+                <button class="`w-full -m-2 p-2" @click="goProduct">商品</button>
+            </div>
+            <div class="flex justify-center flex-grow-0 w-full border border-slate-50 shadow bg-white p-2 rounded-md">
+                <button class="w-full -m-2 p-2" @click="goStock">存貨</button>
+            </div>
+            <div class="flex justify-center flex-grow-0 w-full border border-slate-50 shadow bg-white p-2 rounded-md">
+                <button class="w-full -m-2 p-2" @click="goSetting">設定</button>
+            </div>
+        </div>
+        <div class="my-4 border border-slate-50 shadow bg-white p-2 rounded-md h-48">{{ section }}</div>
     </div>
-    <button @click="add">Add</button>
 </template>
 
 <script>
+import Header from '/src/components/Header.vue';
+
 export default {
     data: () => ({
         x: 1111,
         test: "testaaa",
+        section: "",
     }),
     methods: {
         add: function () {
             this.x += 100;
+        },
+        goProduct: function () {
+            this.section = "product";
+        },
+        goStock: function () {
+            this.section = "stock";
+        },
+        goSetting: function () {
+            this.section = "setting";
         },
     },
     computed: {
         showAbc: function () {
             return `x>1111, ${this.test}`;
         },
+    },
+    components: {
+        Header,
     },
 }
 </script>
