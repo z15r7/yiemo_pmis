ebf924376b995f95b22e6fb367484fa7ad6c004d
z15r7 <z15r7x@gmail.com>
2023-01-03 14:09:02 +0800

    init backend.

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "build")));
+
+// app.use("/api/test", require("./test"));
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/build/index.html"));
+// });
