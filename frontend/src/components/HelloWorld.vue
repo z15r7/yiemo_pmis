50bf4482515b4db8d6e641c33889036958ce5773
z15r7 <z15r7x@gmail.com>
2023-01-03 18:49:40 +0800

    for practice

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,13 +1,3 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
-</script>
-
 <template>
   <h1>{{ msg }}</h1>
 
@@ -22,7 +12,7 @@ const count = ref(0)
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
+    >create-vue</a
     >, the official Vue + Vite starter
   </p>
   <p>
@@ -32,6 +22,16 @@ const count = ref(0)
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  defineProps({
+    msg: String,
+  })
+
+  const count = ref(0)
+</script>
 
 <style scoped>
 .read-the-docs {
---
+++ frontend/src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="">
+        <p>{{ x }}</p>
+        <p v-if="x===1111">{{ "x===1111" }}</p>
+        <p v-if="x>1111">{{ showAbc }}</p>
+    </div>
+    <button @click="add">Add</button>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        x: 1111,
+        test: "testaaa",
+    }),
+    methods: {
+        add: function () {
+            this.x += 100;
+        },
+    },
+    computed: {
+        showAbc: function () {
+            return `x>1111, ${this.test}`;
+        },
+    },
+}
+</script>
