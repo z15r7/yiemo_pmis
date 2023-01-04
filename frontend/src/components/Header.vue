bf8c0b9dc455a057d669d950dca5835375708f0f
z15r7 <z15r7x@gmail.com>
2023-01-04 14:34:18 +0800

    make header section

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,10 +1,21 @@
 <template>
-    <div class="flex justify-center place-items-center shadow bg-amber-500 p-2 rounded-md">
-        <div>hbg</div>
+    <div class="flex justify-center place-items-center shadow border border-slate-50 bg-white p-2 rounded-md">
+        <div></div>
         <div class="w-full">
             <p class="text-xl font-bold text-center">{{ title }}</p>
         </div>
-        <div>btn</div>
+        <div></div>
+    </div>
+    <div class="flex flex-grow-0">
+        <div class="flex justify-center flex-grow-0 w-full border border-slate-300 bg-white p-2 rounded-l-xl">
+            <button class="`w-full -m-2 p-2">商品</button>
+        </div>
+        <div class="flex justify-center flex-grow-0 w-full border border-slate-300 bg-white p-2">
+            <button class="w-full -m-2 p-2">存貨</button>
+        </div>
+        <div class="flex justify-center flex-grow-0 w-full border border-slate-300 bg-white p-2 rounded-r-xl">
+            <button class="w-full -m-2 p-2">設定</button>
+        </div>
     </div>
 </template>
 
@@ -12,6 +23,14 @@
 export default {
     data: () => ({
         title: "yi.emo_nccu",
+        item: [
+            {name: "商品", link: "product", selected: false},
+            {name: "存貨", link: "stock", selected: false},
+            {name: "設定", link: "setting", selected: false}
+            // {name: "商品", link: "product", func: goProduct()},
+            // {name: "存貨", link: "stock", func: goStock()},
+            // {name: "設定", link: "setting", func: goSetting()}
+        ]
     }),
     methods: {
     },
--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,24 +1,7 @@
 <template>
-    <div class="place-item-center w-4/6 p-8 flex-row flex-grow-0 bg-slate-200 h-screen">
+    <div class="w-full max-w-4xl place-item-center p-8 flex-row flex-grow-0 bg-slate-200 h-screen space-y-4">
         <Header />
-        <div class="my-4 border border-slate-50 shadow bg-white p-2 rounded-md">{{ x }}</div>
-        <div class="my-4 border border-slate-50 shadow bg-white p-2 rounded-md" v-if="x===1111">{{ "x===1111" }}</div>
-        <div class="my-4 border border-slate-50 shadow bg-white p-2 rounded-md" v-if="x>1111">{{ showAbc }}</div>
-        <div class="flex justify-center my-4 shadow bg-blue-500 rounded-md p-2 text-white">
-            <button class="w-full -m-2 p-2" @click="add">add</button>
-        </div>
-        <div class="flex flex-grow-0 space-x-4">
-            <div class="flex justify-center flex-grow-0 w-full border border-slate-50 shadow bg-white p-2 rounded-md">
-                <button class="`w-full -m-2 p-2" @click="goProduct">商品</button>
-            </div>
-            <div class="flex justify-center flex-grow-0 w-full border border-slate-50 shadow bg-white p-2 rounded-md">
-                <button class="w-full -m-2 p-2" @click="goStock">存貨</button>
-            </div>
-            <div class="flex justify-center flex-grow-0 w-full border border-slate-50 shadow bg-white p-2 rounded-md">
-                <button class="w-full -m-2 p-2" @click="goSetting">設定</button>
-            </div>
-        </div>
-        <div class="my-4 border border-slate-50 shadow bg-white p-2 rounded-md h-48">{{ section }}</div>
+        <div class="border border-slate-50 shadow bg-white p-2 rounded-md h-48">{{ section }}</div>
     </div>
 </template>
 
@@ -27,14 +10,9 @@ import Header from '/src/components/Header.vue';
 
 export default {
     data: () => ({
-        x: 1111,
-        test: "testaaa",
         section: "",
     }),
     methods: {
-        add: function () {
-            this.x += 100;
-        },
         goProduct: function () {
             this.section = "product";
         },
@@ -46,9 +24,6 @@ export default {
         },
     },
     computed: {
-        showAbc: function () {
-            return `x>1111, ${this.test}`;
-        },
     },
     components: {
         Header,
